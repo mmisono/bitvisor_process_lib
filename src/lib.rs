799cd7f0217c07e4f4f28d74fdaddacdbd0bc41e
Masanori Misono <m.misono760@gmail.com>
2017-07-27 11:40:12 +0900

    print panic reason

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,17 @@ pub mod syscalls;
 #[macro_use]
 pub mod io;
 
+use core::fmt::Write;
+
 #[lang = "eh_personality"]
 #[no_mangle]
 pub extern "C" fn eh_personality() {}
 
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern "C" fn panic_fmt() -> ! {
-    loop {}
+pub extern "C" fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    println!("program panic at '{}', {}:{}", fmt, file, line);
+    println!("process terminated.");
+    syscalls::exitprocess(1);
+    unreachable!();
 }
